a085b381804c2575029060c69a52d408807b1fc5
Rustem <73857551+RustDzhur@users.noreply.github.com>
2022-10-13 03:42:24 +0300

    header-mobile done

---
+++ src/js/header.js
@@ -0,0 +1,11 @@
+﻿const refs = {
+    navContainer: document.querySelector('.nav-container'),
+    menuBtn: document.querySelector('.menu-btn'),
+}
+
+refs.menuBtn.addEventListener('click', openMenu);
+
+function openMenu () {
+    refs.navContainer.classList.toggle('active');
+    refs.menuBtn.classList.toggle('active');
+}
