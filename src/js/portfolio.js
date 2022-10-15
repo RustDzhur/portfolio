35b6ab97d56cd4a136848b3cddd5d3d2a419d3a9
Rustem <73857551+RustDzhur@users.noreply.github.com>
2022-10-15 04:09:34 +0300

    fix bags

--- src/js/portfolio.js
+++ src/js/portfolio.js
@@ -1,21 +1,9 @@
 ﻿import Splide from '@splidejs/splide';
 
-let page = 0;
 
-window.addEventListener("resize", function() {
-    console.log(page)
-    if (window.innerWidth >= 320) {
-        page = 1;
-    } else if (window.innerWidth >=780) {
-        page = 2;
-    } else if (window.innerWidth >= 1400) {
-        page = 3;
-    }
-  });
-
-
-  new Splide( '.splide', {
-    type   : 'loop',
-    perPage: page,
-  } ).mount();
+  new Splide('.splide', {
+    type: 'loop',
+    // perPage: 1,
+    autoWidth: boolean = true,
+  }).mount();
 
