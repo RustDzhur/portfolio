abfb6696e5ba2caedfb54d53267287b7b48e5577
Rustem <73857551+RustDzhur@users.noreply.github.com>
2022-10-15 04:23:16 +0300

    fix bags slider

--- src/js/portfolio.js
+++ src/js/portfolio.js
@@ -4,6 +4,8 @@
   new Splide('.splide', {
     type: 'loop',
     // perPage: 1,
-    autoWidth: boolean = true,
+    autoWidth: true,
+    easing: 'cubic-bezier(0.25, 1, 0.5, 1)',
+    padding: 100,
   }).mount();
 
