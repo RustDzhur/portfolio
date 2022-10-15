224ab940fd71fe0b9500d6d31df596e15486d7cb
Rustem <73857551+RustDzhur@users.noreply.github.com>
2022-10-15 04:26:49 +0300

    fix bags slider gap 1em

--- src/js/portfolio.js
+++ src/js/portfolio.js
@@ -3,9 +3,7 @@
 
   new Splide('.splide', {
     type: 'loop',
-    // perPage: 1,
     autoWidth: true,
-    easing: 'cubic-bezier(0.25, 1, 0.5, 1)',
-    padding: 100,
+    gap: '1em'
   }).mount();
 
