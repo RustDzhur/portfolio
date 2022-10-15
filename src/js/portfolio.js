527d049d633775728dd4a82c0ea151303de6d992
Rustem <73857551+RustDzhur@users.noreply.github.com>
2022-10-15 03:14:56 +0300

    portfoilio slider in progress

---
+++ src/js/portfolio.js
@@ -0,0 +1,21 @@
+﻿import Splide from '@splidejs/splide';
+
+let page = 0;
+
+window.addEventListener("resize", function() {
+    console.log(page)
+    if (window.innerWidth >= 320) {
+        page = 1;
+    } else if (window.innerWidth >=780) {
+        page = 2;
+    } else if (window.innerWidth >= 1400) {
+        page = 3;
+    }
+  });
+
+
+  new Splide( '.splide', {
+    type   : 'loop',
+    perPage: page,
+  } ).mount();
+
