8a785bfc5eea605bc843b8cb3f42816b2a35b014
Rustem <73857551+RustDzhur@users.noreply.github.com>
2022-10-16 01:43:17 +0300

    fis bags

--- src/js/news.js
+++ src/js/news.js
@@ -35,7 +35,7 @@ async function getResponse () {
 getResponse ()
 
 async function onRengerNews (news) {
-    const markupNews = news.map(news => {
+    const markupNews = await news.map(news => {
         return `<li class="splide__slide">
         <a class="card-news__link" href="${news.url}" target="_blanc">
             <div class="card-news__wrapper">
