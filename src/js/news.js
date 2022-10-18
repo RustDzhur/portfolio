b2c7fdf251f61647812b1ed7a3877463712f5cfe
RustDzhur <solar.rezerv@gmail.com>
2022-10-18 22:04:53 +0300

    REST API fixed bugs

--- src/js/news.js
+++ src/js/news.js
@@ -25,11 +25,21 @@ refs.nextBtn.addEventListener('click', (e) => {
 
 function getApiNews () {
     
-    const options = {
-        API_KEY: '090dd760c7d8494cbdd59210368bc828',
-        URL: 'https://newsapi.org/v2/everything',
-    }
-    axios(`${options.URL}?q=IT&apiKey=${options.API_KEY}&pageSize=4&page=${PAGE}`).then(response => onRengerNews(response.data.articles)).catch(err => console.log(err))
+    var options = {
+        method: 'GET',
+        url: 'https://api.newscatcherapi.com/v2/search',
+        params: {q: 'Computer', lang: 'en', sort_by: 'relevancy', page: PAGE, page_size: '4'},
+        headers: {
+          'x-api-key': 'ZZ0_Blzu7KNRL3yZlIdEd030ur1UsnxRdkO9TTAV-h0'
+        }
+      };
+      
+      axios.request(options).then(function (response) {
+          console.log(response.data);
+          onRengerNews (response.data.articles)
+      }).catch(function (error) {
+          console.error(error);
+      });
 }
 
 getApiNews () 
@@ -37,20 +47,20 @@ getApiNews ()
 function onRengerNews (news) {
     const markupNews = news.map(news => {
         return `<li class="splide__slide">
-        <a class="card-news__link" href="${news.url}" target="_blanc">
+        <a class="card-news__link" href="${news.link}" target="_blanc">
             <div class="card-news__wrapper">
                 <h3 class="card-news__title">
                 ${news.title}
                 </h3>
-                <img class="card-news__img" src="${news.urlToImage}" loading="lazy" alt="">
+                <img class="card-news__img" src="${news.media}" loading="lazy" alt="">
                 <p class="card-news__disr">
-                ${news.description}
+                ${news.summary}
                 </p>
                 <p class="card-news__author">
-                ${news.author}
+                ${news.authors}
                 </p>
                 <p class="card-news__date">
-                ${news.publishedAt}
+                ${news.published_date}
                 </p>
             </div>
         </a>
