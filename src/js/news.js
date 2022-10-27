fa724fb49ad13fb3e3d451b6ab6a0b394bff49e6
Rustem <73857551+RustDzhur@users.noreply.github.com>
2022-10-28 02:59:37 +0300

    fix bugs

--- src/js/news.js
+++ src/js/news.js
@@ -2,73 +2,65 @@
 let PAGE = 1;
 
 const refs = {
-    ul: document.querySelector('.js-news-render'),
-    previousBtn: document.querySelector('.previous-btn'),
-    nextBtn: document.querySelector('.next-btn'),
+  ul: document.querySelector('.js-news-render'),
+  previousBtn: document.querySelector('.previous-btn'),
+  nextBtn: document.querySelector('.next-btn'),
 };
 
-refs.previousBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    if (PAGE >=2) {
-        PAGE -= 1;
-        getApiNews ()
-        clearMarkup()
-    }
+refs.previousBtn.addEventListener('click', e => {
+  e.preventDefault();
+  if (PAGE >= 2) {
+    PAGE -= 1;
+    getApiNews();
+    clearMarkup();
+  }
 });
 
-refs.nextBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    PAGE += 1;
-    getApiNews ()
-    clearMarkup()
+refs.nextBtn.addEventListener('click', e => {
+  e.preventDefault();
+  PAGE += 1;
+  getApiNews();
+  clearMarkup();
 });
 
-function getApiNews () {
-    
-    var options = {
-        method: 'GET',
-        url: 'https://api.newscatcherapi.com/v2/search',
-        params: {q: 'Computer', lang: 'en', sort_by: 'relevancy', page: PAGE, page_size: '4'},
-        headers: {
-          'x-api-key': 'ZZ0_Blzu7KNRL3yZlIdEd030ur1UsnxRdkO9TTAV-h0'
-        }
-      };
-      
-      axios.request(options).then(function (response) {
-          console.log(response.data);
-          onRengerNews (response.data.articles)
-      }).catch(function (error) {
-          console.error(error);
-      });
+function getApiNews() {
+  axios
+    .get(
+      'https://newsapi.org/v2/everything?q=Computer&sortBy=popularity&pageSize=4&apiKey=090dd760c7d8494cbdd59210368bc828'
+    )
+    .then(response => {
+        onRengerNews(response.data.articles)
+    });
 }
+getApiNews();
 
-getApiNews () 
-
-function onRengerNews (news) {
-    const markupNews = news.map(news => {
-        return `<li class="splide__slide">
-        <a class="card-news__link" href="${news.link}" target="_blanc">
+function onRengerNews(news) {
+  const markupNews = news
+    .map(({ url, title, urlToImage, description, author, publishedAt }) => {
+      return `<li class="splide__slide">
+        <a class="card-news__link" href="${url}" target="_blanc">
             <div class="card-news__wrapper">
                 <h3 class="card-news__title">
-                ${news.title}
+                ${title}
                 </h3>
-                <img class="card-news__img" src="${news.media}" loading="lazy" alt="">
+                <img class="card-news__img" src="${urlToImage}" loading="lazy" alt="">
                 <p class="card-news__disr">
-                ${news.summary}
+                ${description}
                 </p>
                 <p class="card-news__author">
-                ${news.authors}
+                ${author}
                 </p>
                 <p class="card-news__date">
-                ${news.published_date}
+                ${publishedAt}
                 </p>
             </div>
         </a>
-    </li>`
-    }).join('');
-    refs.ul.insertAdjacentHTML('beforeend', markupNews);
-};
+    </li>`;
+    })
+    .join('');
+  refs.ul.insertAdjacentHTML('beforeend', markupNews);
+}
 
-function clearMarkup () {
-    refs.ul.innerHTML = '';
+function clearMarkup() {
+  refs.ul.innerHTML = '';
 }
